a5f80331fe8feb02f6b297a8ce9c17a57a8d2e49
Matheo Schonborn Kinderknecht <matheoskinder@gmail.com>
2024-06-25 13:23:57 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Declaramos elementos del DOM
+const inputDollar = document.getElementById('input-dolar');
+const texto = document.getElementById('cambio');
+
+// Conseguimos el valor del dolar actualizado desde DolarAPI
+async function getDollarValueFromApi() {
+  const response = await fetch("https://dolarapi.com/v1/dolares/tarjeta");
+  const data = await response.json();
+  return parseInt(data.venta);
+}
+
+async function calculateValue() {
+  // Variable con el valor actualizado del dolar
+  const dollarValue = await this.getDollarValueFromApi();
+
+  // Damos funcion a la entrada numerica
+  inputDollar.addEventListener('change', (e) => {
+    let entrada = e.target.value;
+    let cambio = entrada * dollarValue;
+
+    // Actualizamos el resultado solamente si el usuario ingreso un dato
+    if (entrada !== "") {
+      texto.innerText = `${entrada} USD = ${cambio} ARS \n (Cambio Blue)`;
+    }
+  });
+}
+calculateValue();
+
